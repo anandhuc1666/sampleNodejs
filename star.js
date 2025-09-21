65d11ea4662ab4df5f4d994e232d7398647fe9eb
anandhuc1666 <anandhuc166@gmail.com>
2025-09-21 19:11:28 +0530

    Newnode

---
+++ star.js
@@ -0,0 +1,19 @@
+
+
+// let star = "*"
+
+// for(let i = 1; i<=6;i++){
+//     let patten = ""
+//     for(let j = 1; j<=1; j++){
+//         patten +=star
+//     }
+//     console.log(patten)
+// }
+// for(let i = 6; i>=1;i--){
+//     let patten = ""
+//     for(let j = 1; j<=1; j++){
+//         patten +=star
+//     }
+//     console.log(patten)
+// }
+
