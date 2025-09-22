a754d7d3e76ab2a5268d9a3ef9158bb077e5d802
anandhuc1666 <anandhuc166@gmail.com>
2025-09-22 11:50:29 +0530

    Newnode

--- star.js
+++ star.js
@@ -1,29 +1,11 @@
-const path = require("path");
-const fs = require("fs");
-const { question } = require("readline-sync");
+const Eventemiter = require('events')
+const event = new Eventemiter()
 
-// fs.mkdir(path.join(__dirname,"app"),{},(err)=>{
-//     if(err) throw err
-// })
-// fs.mkdir(path.join(__dirname,"/app/index.text"),{},(err)=>{ ///not currect way to create a file in side a folder(use .txt)
-//     if(err) throw err
-// })
-
-// fs.rmdir(path.join(__dirname,"/app/index.text"),(err)=>{
-//     if(err) throw err
-// })
-// fs.writeFile(path.join(__dirname,"/app","index.text"),"name: anandhu",(err)=>{
-//     if(err) throw err
-// })
-// const clintTxt = question("enter a name:")
-// fs.writeFile(path.join(__dirname,"/app","index.txt"),`your name is: ${clintTxt}`,(err)=>{
-//   if(err) throw err
-// })
-// fs.appendFile(path.join(__dirname,"/app/index.txt"),`\n your name is: ${clintTxt}`,(err)=>{
-//     if(err) throw err
-// })
-fs.readFile(path.join(__dirname,"/app/index.txt"),'utf8',(err,data)=>{
-    if(err) throw err
+event.on('message',(data)=>{
+    console.log(data.use)
+})
+event.on('logout',(data)=>{
     console.log(data)
 })
-
+event.emit('message',{use: 'login'})
+event.emit('logout',{user: 'logout'})
