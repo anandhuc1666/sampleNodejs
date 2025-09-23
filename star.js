68d8bb05dd1ee04a3bd929078860b0b6623f42ce
anandhuc1666 <anandhuc166@gmail.com>
2025-09-23 12:17:42 +0530

    Node

--- star.js
+++ star.js
@@ -1,11 +1,32 @@
-const Eventemiter = require('events')
-const event = new Eventemiter()
+// const Eventemiter = require('events')
+// const event = new Eventemiter()
 
-event.on('message',(data)=>{
-    console.log(data.use)
-})
-event.on('logout',(data)=>{
-    console.log(data)
-})
-event.emit('message',{use: 'login'})
-event.emit('logout',{user: 'logout'})
+// event.on('message',(data)=>{
+//     console.log(data.use)
+// })
+// event.on('logout',(data)=>{
+//     console.log(data)
+// })
+// event.emit('message',{use: 'login'})
+// event.emit('logout',{user: 'logout'})
+// const path = require('path')
+// const fs = require('fs')
+// const http = require("http")
+// fs.readFile(path.join(__dirname, "/app/index.txt"), "utf8", (err, data) => {
+//     if (err) throw err
+//     if (data) {
+//         const server = http.createServer((req, res) => {
+//             console.log(req.url)
+//             res.write(data)
+//             res.end()
+//         })
+//         const PORT = process.env.PORT || 3001;
+//         server.listen(PORT, () => console.log(`server is running in ${PORT}`))
+//     } else {
+//         console.log("server not running")
+//     }
+// })
+
+ 
+
+
