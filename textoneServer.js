5ca364a597584e9ec7df102e76befa7ba0565b8c
anandhuc1666 <anandhuc166@gmail.com>
2025-09-24 12:19:02 +0530

    NodeCode

--- textoneServer.js
+++ textoneServer.js
@@ -1,1 +1,11 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+http.createServer((req, res) => {
+    fs.readFile(path.join(__dirname, "/app", "index.txt"), "utf8", (err, data) => {
+        if(err) throw err
+        res.write(data)
+        res.end()
+    })
+}).listen(5005, () => console.log('server is on'))
--- texttwo.js
+++ texttwo.js
@@ -1,7 +1,14 @@
 const path = require('path')
 const fs = require('fs')
-const stud = 'leave'
-fs.appendFile(path.join(__dirname,'/app','index.txt'),`\n arun:${stud}`,(err)=>{
-   if(err) throw err
-   console.log('msg send/...')
+// const stud = 'leave'
+// fs.appendFile(path.join(__dirname,'/app','index.txt'),`\n arun:${stud}`,(err)=>{
+//    if(err) throw err
+//    console.log('msg send/...')
+// })
+
+const read = fs.readFile(path.join(__dirname,"/app",'index.txt') , "utf8",(err,data)=>{
+    if(err) throw err
+    console.log(data)
 })
+
+console.log(this.data)
