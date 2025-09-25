7828c18d710e1cefea0b7d336ea44db0d0666928
anandhuc1666 <anandhuc166@gmail.com>
2025-09-25 10:13:57 +0530

    CodeNode

--- page/serverCreate.js
+++ page/serverCreate.js
@@ -2,19 +2,6 @@ const path = require('path')
 const fs = require('fs')
 const http = require('http')
 
-
-http.createServer((req, res) => {
-    fs.writeFile('server1.txt', "hello text one", (err) => {
-        if (err) throw err
-        return
-    })
-    fs.readFile('server1.txt','utf8',(err,data)=>{
-        if(err) throw err
-        res.writeHead(200,{"content-Type":"html/text"})
-        console.log('file readed....')
-        console.log(data)
-        res.write(data)
-        res.end()
-        return
-    })
-}).listen(5005, () => console.log('server running...'))
+fs.unlink('re-Text.txt',(err)=>{
+    if(err) throw err
+})
