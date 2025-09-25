e91de18e1d8d432d0b50d4d5277468c7d19b95f5
anandhuc1666 <anandhuc166@gmail.com>
2025-09-25 10:06:02 +0530

    NodeCode

--- page/serverCreate.js
+++ page/serverCreate.js
@@ -1,7 +1,20 @@
-
+const path = require('path')
+const fs = require('fs')
 const http = require('http')
 
-http.createServer((req,res)=>{
-   res.write('hello my new project')
-   res.end()
-}).listen(5005,()=>console.log('server running...'))
+
+http.createServer((req, res) => {
+    fs.writeFile('server1.txt', "hello text one", (err) => {
+        if (err) throw err
+        return
+    })
+    fs.readFile('server1.txt','utf8',(err,data)=>{
+        if(err) throw err
+        res.writeHead(200,{"content-Type":"html/text"})
+        console.log('file readed....')
+        console.log(data)
+        res.write(data)
+        res.end()
+        return
+    })
+}).listen(5005, () => console.log('server running...'))
