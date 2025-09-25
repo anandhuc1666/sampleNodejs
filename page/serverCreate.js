19589b733ecf069c6c7fdc64b2e9f3a94534c1dc
anandhuc1666 <anandhuc166@gmail.com>
2025-09-25 09:47:53 +0530

    NodeCode

--- page/serverCreate.js
+++ page/serverCreate.js
@@ -1,3 +1,7 @@
-const path = require('path')
+
 const http = require('http')
 
+http.createServer((req,res)=>{
+   res.write('hello my new project')
+   res.end()
+}).listen(5005,()=>console.log('server running...'))
