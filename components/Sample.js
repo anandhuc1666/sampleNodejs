d86d38be53ed590ba6e9461eca8d801b9ef03778
anandhuc1666 <anandhuc166@gmail.com>
2025-09-29 14:49:09 +0530

    NodeCode

--- components/Sample.js
+++ components/Sample.js
@@ -1,17 +1,25 @@
-const http = require('http');
+// const http = require('http');
+// const fs = require('fs')
+
+// http.createServer((req,res)=>{
+//     fs.writeFile('sample.txt','hello new content on writting',(err)=>{
+//         if(err) throw err
+//         fs.readFile('sample.txt','utf8',(err,data)=>{
+//             console.log(`data reading ${data}`)
+//             res.writeHead(200,{"content-type":"text/plain"})
+
+//             res.end(data)
+//         })
+//     })
+//     fs.appendFile('sample.txt','\nadd next content on the file',(err)=>{
+//         if(err) throw err
+//     })
+// }).listen(5000,()=>console.log('server is on '));
+
+const http = require('http')
 const fs = require('fs')
 
 http.createServer((req,res)=>{
-    fs.writeFile('sample.txt','hello new content on writting',(err)=>{
-        if(err) throw err
-        fs.readFile('sample.txt','utf8',(err,data)=>{
-            console.log(`data reading ${data}`)
-            res.writeHead(200,{"content-type":"text/plain"})
-            
-            res.end(data)
-        })
-    })
-    fs.appendFile('sample.txt','\nadd next content on the file',(err)=>{
-        if(err) throw err
-    })
-}).listen(5000,()=>console.log('server is on '));
+    
+    fs.writeFile('sample.txt',)
+})
