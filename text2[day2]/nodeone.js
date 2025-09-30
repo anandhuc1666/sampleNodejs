6f7320165db49dc4c5157d3f1bd2b0a2bf855fd9
anandhuc1666 <anandhuc166@gmail.com>
2025-09-30 12:20:55 +0530

    Your commit message

--- text2[day2]/nodeone.js
+++ text2[day2]/nodeone.js
@@ -1,19 +1,18 @@
-const fs = require('fs').promises
-const http = require('http')
+// const fs = require('fs').promises
+// const http = require('http')
 
-http.createServer(async (req, res) => {
-    try {
-        await fs.writeFile('text1.txt', 'text is writing')
-        const data = await fs.readFile('text1.txt', 'utf8')
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(data)
-        res.write("<h1> wellcome user </h1>")
-        res.end()
-    } catch (err) {
-        console.log(err)
-        res.writeHead(500, { 'content-type': 'text/html' })
-        res.end()
-    }
+// http.createServer(async (req, res) => {
+//     try {
+//         await fs.writeFile('text1.txt', 'text is writing')
+//         const data = await fs.readFile('text1.txt', 'utf8')
+//         res.writeHead(200, { 'content-type': 'text/html' })
+//         res.write(data)
+//         res.write("<h1> wellcome user </h1>")
+//         res.end()
+//     } catch (err) {
+//         console.log(err)
+//         res.writeHead(500, { 'content-type': 'text/html' })
+//         res.end()
+//     }
 
-}).listen(5000, () => console.log('server is on running 5000 port'))
-
+// }).listen(5000, () => console.log('server is on running 5000 port'))
