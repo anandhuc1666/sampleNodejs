7d6ce983120d97c54a1efa799480f08deca4e633
anandhuc1666 <anandhuc166@gmail.com>
2025-09-30 10:38:27 +0530

    codeNode

---
+++ text2[day2]/nodeone.js
@@ -0,0 +1,12 @@
+// const fs =require('fs')
+// const path =require('path')
+// const http = require('http')
+// const numbers = 10
+// for(let i = 0; i<=numbers;i++){
+// fs.unlink(`text${i}.txt`,(err)=>{
+//     if(err) throw err
+// })
+// }
+
+
+
