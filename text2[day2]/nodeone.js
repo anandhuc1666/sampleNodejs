ddd1614603ba459cab5773a9393e59efe8e9388f
anandhuc1666 <anandhuc166@gmail.com>
2025-09-30 11:04:01 +0530

    codeNode

--- text2[day2]/nodeone.js
+++ text2[day2]/nodeone.js
@@ -1,24 +1,19 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const http = require('http')
 
-http.createServer((req,res)=>{
-    fs.writeFile('text.txt','hello new file',(err)=>{
-        if(err) throw err
-    })
-    const readData = async()=>{
-        try{
-             const data = await fs.readFile('text.txt','utf8',()=>{
-                console.log(data)
-                res.writeHead(200, {'content-type':'text/html'})
-                res.write(data)
-                res.end()
-             })
-        }catch (err){
-          console.log(err)
-        }
-      
+http.createServer(async (req, res) => {
+    try {
+        await fs.writeFile('text1.txt', 'text is writing')
+        const data = await fs.readFile('text1.txt', 'utf8')
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(data)
+        res.write("<h1> wellcome user </h1>")
+        res.end()
+    } catch (err) {
+        console.log(err)
+        res.writeHead(500, { 'content-type': 'text/html' })
+        res.end()
     }
-    readData()
 
-}).listen(5000,()=>console.log('server is on running 5000 port'))
+}).listen(5000, () => console.log('server is on running 5000 port'))
 
