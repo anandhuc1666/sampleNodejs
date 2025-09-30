aaa9f6dd71a4e78561896ffd9c427a7b43b611cc
anandhuc1666 <anandhuc166@gmail.com>
2025-09-30 10:55:27 +0530

    Codenode

--- text2[day2]/nodeone.js
+++ text2[day2]/nodeone.js
@@ -1,5 +1,24 @@
 const fs = require('fs')
 const http = require('http')
 
+http.createServer((req,res)=>{
+    fs.writeFile('text.txt','hello new file',(err)=>{
+        if(err) throw err
+    })
+    const readData = async()=>{
+        try{
+             const data = await fs.readFile('text.txt','utf8',()=>{
+                console.log(data)
+                res.writeHead(200, {'content-type':'text/html'})
+                res.write(data)
+                res.end()
+             })
+        }catch (err){
+          console.log(err)
+        }
+      
+    }
+    readData()
 
+}).listen(5000,()=>console.log('server is on running 5000 port'))
 
