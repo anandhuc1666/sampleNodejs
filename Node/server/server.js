eefad76466a1b62271be45af67933a741527a117
anandhuc1666 <anandhuc166@gmail.com>
2025-10-03 11:31:13 +0530

    CodeNode

--- Node/server/server.js
+++ Node/server/server.js
@@ -36,3 +36,4 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => console.log(`server is running in ${PORT}`))
+
---
+++ Node/server/text.js
@@ -0,0 +1,4 @@
+const logger = () => {
+    return sample.html
+}
+module.exports = logger;
