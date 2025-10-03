c3c5814c687fcbdf15338ed9351ea8650b940295
anandhuc1666 <anandhuc166@gmail.com>
2025-10-03 10:53:05 +0530

    NodeCode

--- Node/server/server.js
+++ Node/server/server.js
@@ -1,10 +1,29 @@
+// const http = require('http')
+// const fs = require('fs')
+
+// http.createServer((req,res)=>{
+//     fs.readFile('sample.html','utf8',(err,data)=>{
+//         if(err) throw err
+//         res.write(data)
+//         res.end()
+//     })
+// }).PORT
+// const PORT = process.env.PORT || 3000
 const http = require('http')
 const fs = require('fs')
-
-http.createServer((req,res)=>{
-    fs.readFile('sample.html','utf8',(err,data)=>{
-        if(err) throw err
+const server = http.createServer((req, res) => {
+    fs.readFile('sample.html', 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'content-type': 'text/html' })
+            res.end('<h1>server not response 404</h1>')
+            console.log(err)
+            return
+        }
+        res.writeHead(200, { 'content-type': 'text/html' })
         res.write(data)
         res.end()
     })
-}).listen(3000,()=>console.log('server is running port 3000'))
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT,()=>console.log(`server is running in ${PORT}`))
