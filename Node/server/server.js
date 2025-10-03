12bae2d339fd15ded2dd80c0ae3b8f9c9ba85868
anandhuc1666 <anandhuc166@gmail.com>
2025-10-03 11:10:56 +0530

    CodeNode

--- Node/server/server.js
+++ Node/server/server.js
@@ -11,19 +11,28 @@
 // const PORT = process.env.PORT || 3000
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
+
 const server = http.createServer((req, res) => {
-    fs.readFile('sample.html', 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, 'sample.html'), 'utf8', (err, data) => {
         if (err) {
             res.writeHead(500, { 'content-type': 'text/html' })
             res.end('<h1>server not response 404</h1>')
             console.log(err)
             return
         }
-        res.writeHead(200, { 'content-type': 'text/html' })
-        res.write(data)
-        res.end()
+        if (req.url === "/") {
+            res.writeHead(200, { 'content-type': 'text/html' })
+            res.write(data)
+            res.end()
+        }else{
+            res.writeHead(200,{'content-type':'text/html'})
+            res.write('<h1> this page not available </h1>')
+            res.end()
+        }
+
     })
 })
 
 const PORT = process.env.PORT || 3000
-server.listen(PORT,()=>console.log(`server is running in ${PORT}`))
+server.listen(PORT, () => console.log(`server is running in ${PORT}`))
