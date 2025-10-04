5a6ed745e157f74e1ab0036842087cbccd874f08
anandhuc1666 <anandhuc166@gmail.com>
2025-10-04 13:18:54 +0530

    CodeNode

--- form/form.js
+++ form/form.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 const http = require('http')
-const url =require('url')
+const url = require('url')
 
 http.createServer((req, res) => {
-    let q = url.parse(req.url)
-    if (req.url === '/') {
+    let q = url.parse(req.url,true)
+    if (q.pathname === '/') {
         fs.readFile('form.html', 'utf8', (err, data) => {
             if (err) {
                 console.log(err)
@@ -14,9 +14,15 @@ http.createServer((req, res) => {
             res.write(data)
             res.end()
         })
-    }else if('/home'){
+    }else if(q.query.name === 'anandhu' && q.query.password==="123456") {
         res.write('welcome home')
         res.end()
+    }else{
+       res.write('pass not found')
+       res.end() 
     }
+
+
+
 
 }).listen(3000, () => console.log('server is running 3000'))
