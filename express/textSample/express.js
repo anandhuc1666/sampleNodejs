5b1d2beb8c2399098954309f317990fba5e06162
anandhuc1666 <anandhuc166@gmail.com>
2025-10-07 14:33:47 +0530

    CodeExpress

--- express/textSample/express.js
+++ express/textSample/express.js
@@ -13,15 +13,21 @@ const users = [
 app.get('/',(req,res)=>{
    return res.send('create get users')
 })
-app.post('/users',(req,res)=>{
+app.get('/users',(req,res)=>{
   return  res.json(users)
 })
 
-app.post('/users/:id',(req,res)=>{
-    const {id} = req.params
-    console.log(id)
-   return res.json(users.find((user)=>user.id === id))
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params
+  const user = users.find(u => u.id === Number(id))
+  res.json(user)
 })
 
+app.post('/users',(req,res)=>{
+  const {name,email}=req.body;
+  let user = {id:users.length + 1,name,email}
+  users.push(user)
+  res.json(user)
+})
 
 app.listen(PORT,()=>console.log('server is on'))
