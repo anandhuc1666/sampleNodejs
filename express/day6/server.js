59d186594d3228ed8ab813890b04746ff055bb36
anandhuc1666 <anandhuc166@gmail.com>
2025-10-11 09:49:00 +0530

    ExpressMongodb

--- express/day6/server.js
+++ express/day6/server.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 
 const PORT = 3000
+let users =[]
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get('/users',(req,res)=>{
+    res.json(users)
+})
+app.post('/users',(req,res)=>{
+    const {name,email}=req.body
+    const newUser = {id:users.length+1,name,email}
+    res.json(newUser)
+})
+app.get('/users/:id',(req,res)=>{
     
 })
