15ce0ba4615c3593819b7c391ae6968281da7e0c
anandhuc1666 <anandhuc166@gmail.com>
2025-10-11 10:10:48 +0530

    ExpressMongodb

--- express/day6/server.js
+++ express/day6/server.js
@@ -11,6 +11,7 @@ app.get('/users',(req,res)=>{
 app.post('/users',(req,res)=>{
     const {name,email}=req.body
     const newUser = {id:users.length+1,name,email}
+    users.push(newUser)
     res.json(newUser)
 })
 app.get('/users/:id',(req,res)=>{
@@ -28,4 +29,17 @@ app.delete('/users/:id',(req,res)=>{
 app.patch('/users/:id',(req,res)=>{
     const {id}=req.params
     const newId = Number(id)
+    const newUser = users.find((u)=>u.id === newId)
+    const {name,email}=req.body
+    if(name){
+       newUser.name = name
+    }
+    if(email){
+        newUser.email = email
+    }
+    const index = users.findIndex((i)=>i === newId)
+    users[index] = newUser
+    res.json(newUser)
 })
+
+app.listen(PORT,()=>console.log('server is running on'))
