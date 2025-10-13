3c5e5e8c6bdbfc495bdb26d7afa09820c3da59a8
anandhuc1666 <anandhuc166@gmail.com>
2025-10-13 10:00:07 +0530

    ExpressMongodb

--- express/server/server.js
+++ express/server/server.js
@@ -5,45 +5,45 @@ const PORT = 3000
 app.use(express.json())
 let users = []
 
-app.get('/users',(req,res)=>{
-res.json(users)
+app.get('/users', (req, res) => {
+    res.json(users)
 })
 
-app.post('/users',(req,res)=>{
-    const {name,email}= req.body
-    const user = {id:users.length +1,name,email}
+app.post('/users', (req, res) => {
+    const { name, email } = req.body
+    const user = { id: users.length + 1, name, email }
     users.push(user)
     res.json(user)
 })
 
-app.get('/users/:id',(req,res)=>{
-    const {id} = req.params
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params
     const userID = Number(id)
-    const user = users.find((u)=>u.id === userID)
+    const user = users.find((u) => u.id === userID)
     res.json(user)
 })
 
-app.delete('/users/:id',(req,res)=>{
-    const {id} = req.params
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params
     const userID = Number(id)
-    const user = users.find((u)=>u.id === userID)
-    users = users.filter((u)=>u.id !== user.id)
+    const user = users.find((u) => u.id === userID)
+    users = users.filter((u) => u.id !== user.id)
     res.json(users)
 })
-app.patch('/users/:id',(req,res)=>{
-    const {id} = req.params
+app.patch('/users/:id', (req, res) => {
+    const { id } = req.params
     const userID = Number(id)
-    const user = users.find((u)=>u.id === userID)
-    const {name,email}= req.body
-    if(name){
-        user.name =name
+    const user = users.find((u) => u.id === userID)
+    const { name, email } = req.body
+    if (name) {
+        user.name = name
     }
-    if(email){
+    if (email) {
         user.email = email
     }
-    const index = users.indexOf((i)=>i.id === userID)
-    users[index]=user
+    const index = users.indexOf((i) => i.id === userID)
+    users[index] = user
     res.json(user)
 })
 
-app.listen(PORT,()=>console.log('server is on running'))
+app.listen(PORT, () => console.log('server is on running'))
