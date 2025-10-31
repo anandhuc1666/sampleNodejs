f5d771192526752c70aed08ae74cf2f660ac92f5
anandhuc1666 <anandhuc166@gmail.com>
2025-10-31 15:40:00 +0530

    ExpressMongodb

--- auth/src/Login.jsx
+++ auth/src/Login.jsx
@@ -1,45 +1,60 @@
+import React, { useState } from "react";
 import axios from "axios";
-import React from "react";
-import { useState } from "react";
 
 function Login() {
   const [int, setInt] = useState({
     email: "",
     password: "",
   });
+
   const handleChange = (e) => {
-    e.preventDefault();
     setInt({ ...int, [e.target.name]: e.target.value });
   };
-  const handleSubmit = () => {
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); 
+
     try {
-      const response = axios(`http://localhost:3000/user/register`, int);
-      alert("user login successfully")
-        .then(() => console.log(response.data))
-        .catch((error) => console.log(error));
+      const response = await axios.post("http://localhost:3000/user/login", int);
+      alert("User login successfully");
+      console.log(response.data);
     } catch (error) {
-      console.log(error);
+      console.error("Error:", error.response ? error.response.data : error.message);
     }
   };
-  console.log(int);
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          placeholder="enter your email"
-          value={int.email}
+          placeholder="Enter your email"
           name="email"
+          value={int.email}
           onChange={handleChange}
         />
         <input
-          type="text"
-          placeholder="enter your password"
-          value={int.password}
+          type="password"
+          placeholder="Enter your password"
           name="password"
+          value={int.password}
           onChange={handleChange}
         />
-        <button onClick={handleSubmit}>login</button>
+        {/* <input
+          type="text"
+          placeholder="Enter your name"
+          name="name"
+          value={int.name}
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="Enter your number"
+          name="number"
+          value={int.number}
+          onChange={handleChange}
+        /> */}
+        <button type="submit">Login</button>
       </form>
     </div>
   );
