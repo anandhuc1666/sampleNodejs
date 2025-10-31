f6d4d148d6d9956435abcb653efb47805ef92069
anandhuc1666 <anandhuc166@gmail.com>
2025-10-31 13:38:22 +0530

    ExpressMongodb

--- express/day1/sample.js
+++ express/day1/sample.js
@@ -9,13 +9,14 @@
 // app.get('/',(req,res)=>{
 //    res.sendFile(path.join(__dirname,'sample.html'))
 // })
+
 // app.post('/',(req,res)=>{
 //    let Nuser = req.body
 //     res.send(`new user name : ${Nuser.name} <br> new user password : ${Nuser.password}` )
 //     console.log(Nuser.name ,Nuser.password)
 // })
-
 // app.listen(PORT,()=>{
 //     console.log(`sever is running port ${PORT}`)
 // })
 
+const http =  require('http')
