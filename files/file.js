5840e7a9403e99fa623b10321ab2beac49397c67
anandhuc1666 <anandhuc166@gmail.com>
2025-11-04 15:15:29 +0530

    ExpressMongodb

---
+++ files/file.js
@@ -0,0 +1,17 @@
+import express from "express"
+import multer from "multer"
+
+const app = express()
+
+const storage = multer.diskStorage({
+    destination:"./upload",
+    filename:(req,res,cd)=>cd(null,"test.jpg")
+})
+
+const uploader = multer({storage})
+app.post("/file",uploader.single("file"),(req,res)=>{
+    res.send("send dome")
+})
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT,()=>console.log(`server is rinning on ${PORT}`))
