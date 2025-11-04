e63009eb3ae6dfc70798ac7e68358e9a404b895e
anandhuc1666 <anandhuc166@gmail.com>
2025-11-04 14:40:17 +0530

    ExpressMongodb

--- crud/router/usersRoute.js
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express"
-import { login, register } from "../../crud/controller/users.js"
-
-const route = Router()
-
-route.post("/register",register).post("/login",login)
-
-export default route
